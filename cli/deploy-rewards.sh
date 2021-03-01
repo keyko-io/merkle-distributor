3960ac85c983e122fdde13844b748a0b0e175b30
Emily Williams <emag3m@gmail.com>
2021-03-01 16:37:34 -0500

    progress

--- cli/deploy-rewards.sh
+++ cli/deploy-rewards.sh
@@ -19,12 +19,12 @@ echo "\tprivateKey:" $5
 export NODE_OPTIONS="--max-old-space-size=8192"
 
 # get blocks until March. Cache and comment out next 2 lines for faster runtime next session.
-echo "\nFetching Rewards until February 1st"
+# echo "\nFetching Rewards until February 1st"
 celogive rewards:fetchevents --toDate 02/01/2021 --env mainnet
 
-# last block fetched for date 02/01/2021 is 391825
+# last block fetched for date 02/01/2021 is 4927505
 echo "\nFetching rest of rewards..."
-celogive rewards:fetchevents --fromBlock 391826 --toDate $2 --env mainnet
+celogive rewards:fetchevents --fromBlock 4927506 --toDate $2 --env mainnet
 
 # collect event files in proper order to feed as parameters (replaces \n with a space)
 ATTESTATION_EVENTS=$(ls -tr | grep attestation | tr "\n" " ")
@@ -32,6 +32,14 @@ TRANFER_EVENTS=$(ls -tr | grep transfer | tr "\n" " ")
 
 # construct merkle tree and output all intermediary data into rewardsCalculationState.json
 echo "\nGenerating Merkle Tree..."
+echo "celogive rewards:generatemerkle \
+  --attestationEvents  $ATTESTATION_EVENTS \
+  --transferEvents $TRANFER_EVENTS\
+  --balanceFromDate $1 \
+  --balanceToDate $2 \
+  --celoToUsd $3 \
+  --env mainnet\n"
+
 celogive rewards:generatemerkle \
   --attestationEvents  $ATTESTATION_EVENTS \
   --transferEvents $TRANFER_EVENTS\
@@ -46,4 +54,3 @@ celogive rewards:deploydistributor \
   --env mainnet \
   --from $4 \
   --privateKey $5
-   
